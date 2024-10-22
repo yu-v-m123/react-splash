16c58ec54f28189b067e95b73c46f079b76314e4
yu <nagase.yuki0123@gmail.com>
2024-10-23 03:02:15 +0900

    feat: get image data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Header from './components/Header';
+import Search from './components/Search';
+import unsplash from './api/unsplash';
 
 function App() {
+	const onSearchSubmit = async (term) => {
+		const response = await unsplash.get('/search/photos', {
+			params: { query: term }
+		});
+		console.log(response)
+	};
+
 	return (
 		<div className="App">
-			<Header />
+			<Header>
+				<Search onSearchSubmit={onSearchSubmit} />
+			</Header>
 		</div>
 	);
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import './Header.css';
 
-const Header = () => {
+const Header = ({ children }) => {
 	return (
-			<div className="Header">
-				<span className="Header__Logo">
-					TypeScrash
-				</span>
-			</div>
+		<div className="Header">
+			<span className="Header__Logo">
+				TypeSprash
+			</span>
+			{ children }
+		</div>
 	);
 }
 
