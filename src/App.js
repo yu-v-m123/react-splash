0c0b1f0a66fee55fd978c76a4240755f0f057f9e
yu <nagase.yuki0123@gmail.com>
2024-10-27 03:20:51 +0900

    feat: infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
 import React from 'react';
 import Header from './components/Header';
-import Search from './components/Search';
 import unsplash from './api/unsplash';
 import ImageList from './components/ImageList';
+import './components/Search.css';
 
 function App() {
 	const [images, setImages] = React.useState([]);
+	const [term, setTerm] = React.useState('');
+	const [page, setPage] = React.useState(1);
 
-	const onSearchSubmit = async (term) => {
+	const onSearchSubmit = async (term, page) => {
 		const response = await unsplash.get('/search/photos', {
-			params: { query: term }
+			params: {
+				query: term,
+				per_page: 21,
+				page: page,
+			}
 		});
 
-		setImages(response.data.results);
+		// pageが1だったらresponse.data.resultsを返す。
+		// 1以外だったら、imagesに新しく取得したdataを結合する。
+		setImages(
+			page === 1
+				? response.data.results
+				: images.concat(response.data.results)
+		);
+	};
+
+	const onFormSubmit = (event) => {
+		event.preventDefault();
+		setPage(1); // 新しいキーワードで検索した場合、pageを1に設定する。
+		onSearchSubmit(term, 1);
 	};
 
 	return (
 		<div className="App">
 			<Header>
-				<Search onSearchSubmit={onSearchSubmit} />
+				<form onSubmit={onFormSubmit}>
+					<input
+						className='Search'
+						type="text"
+						value={term}
+						placeholder="検索"
+						onChange={e => setTerm(e.target.value)}
+					/>
+				</form>
 			</Header>
 			<div>
-				<ImageList images={images}/>
+				<ImageList images={images} onSearchSubmit={onSearchSubmit} term={term} page={page} setPage={setPage} />
 			</div>
 		</div>
 	);
--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -3,11 +3,29 @@ import './ImageList.css';
 import ImageCard from './ImageCard';
 
 const imageList = (props) => {
+    const handleScroll = (event) => {
+        const { scrollHeight, scrollTop, clientHeight } = event.target;
+
+        if (scrollHeight - scrollTop === clientHeight) {
+            console.log('reach end');
+            const newPage = props.page + 1;
+            props.setPage(newPage);
+            props.onSearchSubmit(props.term, newPage);
+        }
+    };
+
     const images = props.images.map((image) => {
         return <ImageCard key={image.id} image={image} />;
     });
 
-    return <div className="image-list">{images}</div>;
+    return (
+        <div
+            onScroll={handleScroll}
+            className="image-list"
+            style={{ height: '1000px', overflow: 'scroll' }}>
+            {images}
+        </div>
+    );
 };
 
 export default imageList;
